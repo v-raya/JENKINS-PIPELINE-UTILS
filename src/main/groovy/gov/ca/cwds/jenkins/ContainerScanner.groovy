8ddee675a292edce418915bffa7e17d20e88dac8
Ed Gibbs <edward_gibbs@yahoo.com>
2018-10-16 16:52:03 -0700

    First shared container scanner :unicorn:

---
+++ src/main/groovy/gov/ca/cwds/jenkins/ContainerScanner.groovy
@@ -0,0 +1,16 @@
+package gov.ca.cwds.jenkins
+
+class ContainerScanner {
+  def script
+
+  ContainerScanner(script) {
+    this.script = script
+  }
+
+  def sendNotification(containerName, containerVersion) {
+    script.build job: 'tenable-scan', parameters: [
+      [$class: 'StringParameterValue', name: 'CONTAINER_NAME', value: containerName],
+      [$class: 'StringParameterValue', name: 'CONTAINER_VERSION', value: containerVersion]
+    ]
+  }
+}
---
+++ src/test/groovy/gov/ca/cwds/jenkins/ContainerScannerSpecification.groovy
@@ -0,0 +1,30 @@
+package gov.ca.cwds.jenkins
+
+import spock.lang.Specification
+
+class ContainerScannerSpecification extends Specification {
+
+  class PipeLineScript {
+    def build(hash) {
+    }
+
+    def PipeLineScript() { }
+  }
+
+  def "#sendNotification"() {
+    def buildArguments
+
+    given:
+    def pipeline = Mock(PipeLineScript)
+    def containerScanner = new ContainerScanner(pipeline)
+
+    when:
+    containerScanner.sendNotification('appllo', 'a1')
+
+    then:
+    1 * pipeline.build(_) >> { arguments -> buildArguments=arguments[0]}
+    buildArguments['job'] == 'tenable-scan'
+    buildArguments['parameters'][0] == [$class: 'StringParameterValue', name: 'CONTAINER_NAME', value: 'appllo']
+    buildArguments['parameters'][1] == [$class: 'StringParameterValue', name: 'CONTAINER_VERSION', value: 'a1']
+  }
+}
