1a9682027ab1e55d910f41db88db090a94fb0460
Ed Gibbs <edward_gibbs@yahoo.com>
2018-10-19 11:33:06 -0700

    Fixes order on import statement

--- vars/containerScan.groovy
+++ vars/containerScan.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-import ca.gov.cwds.jenkins.ContainerScanner
+import gov.ca.cwds.jenkins.ContainerScanner
 
 def call(String containerName, String containerVersion) {
   containerScanner = new ContainerScanner(this)
