b9219f3fc9545da61ebeea6c3509734199b155b6
Ed Gibbs <edward_gibbs@yahoo.com>
2018-11-02 13:57:06 -0700

    Removes test only string

--- src/gov/ca/cwds/jenkins/PullRequestMergedTrigger.groovy
+++ src/gov/ca/cwds/jenkins/PullRequestMergedTrigger.groovy
@@ -19,7 +19,7 @@ class PullRequestMergedTrigger {
       ],
       regexpFilterText: '$pull_request_action:$trigger_key:$pull_request_merged',
       regexpFilterExpression: '^closed:' + triggerKeyParameter + ':true$',
-      causeString: 'Triggered by PR merge test',
+      causeString: 'Triggered by PR merge',
       printContributedVariables: false
     ]
   }
