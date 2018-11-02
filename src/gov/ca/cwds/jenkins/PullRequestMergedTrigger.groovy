4c1b2958dc6a43018c0f2d200628bf9ddf0ff9b3
Ed Gibbs <edward_gibbs@yahoo.com>
2018-11-02 09:56:45 -0700

    Creates a pull request merged trigger settings for a Jenkins job :unicorn:

---
+++ src/gov/ca/cwds/jenkins/PullRequestMergedTrigger.groovy
@@ -0,0 +1,26 @@
+package gov.ca.cwds.jenkins
+
+class PullRequestMergedTrigger {
+  def script
+
+  PullRequestMergedTrigger(script) {
+    this.script = script
+  }
+
+  def triggerProperties(triggerKeyParameter) {
+    [$class: 'GenericTrigger',
+      genericVariables: [
+        [key: 'pull_request_action', value: 'action'],
+        [key: 'pull_request_merged', value: 'pull_request.merged'],
+        [key: 'pull_request_event', value: 'pull_request']
+      ],
+      genericRequestVariables: [
+        [key: 'trigger_key']
+      ],
+      regexpFilterText: '$pull_request_action:$trigger_key:$pull_request_merged',
+      regexpFilterExpression: '^closed:' + triggerKeyParameter + ':true$',
+      causeString: 'Triggered by PR merge',
+      printContributedVariables: false
+    ]
+  }
+}
