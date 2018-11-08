b94117bf7e41929ae27012dec3b6675c30f68553
Ed Gibbs <edward_gibbs@yahoo.com>
2018-11-08 13:52:59 -0800

    Methods for updating the manifest :unicorn:

---
+++ src/gov/ca/cwds/jenkins/semver/ManifestUpdater.groovy
@@ -0,0 +1,37 @@
+package gov.ca.cwds.jenkins.semver
+
+class ManifestUpdater {
+  def CARES_GITHUB_URL = "git@github.com:ca-cwds/cws-cares.git"
+  def GIT_USER = "Jenkins"
+  def GIT_EMAIL = "cwdsdoeteam@osi.ca.gov"
+  def script
+
+  def ManifestUpdater(script) {
+    this.script = script
+  }
+
+  def update(applicationName, manifestName, credentialsId, version) {
+    checkoutCares(credentialsId)
+    updateManifestFile(applicationName, manifestName, version)
+    script.sshagent(credentials: credentialsId) { commitVersionInCares() }
+  }
+
+  def commitVersionInCares() {
+    if (script.sh(script: "git status --porcelain", returnStdout: true)) {
+      script.sh(script: "git config --global user.email ${GIT_EMAIL}")
+      script.sh(script: "git config --global user.name '${GIT_USER}'")
+      script.sh(script: "git commit -am \"Update component version from script\"")
+      script.sh(script: "git push origin master")
+    }
+  }
+
+  private checkoutCares(credentialsId) {
+    script.git branch: "master", credentialsId: credentialsId, url: CARES_GITHUB_URL
+  }
+
+  private updateManifestFile(applicationName, manifestName, version) {
+    def properties = script.readYaml file: "${manifestName}.yaml"
+    properties."${applicationName}" = version
+    script.writeYaml file: "${manifestName}.yaml", data: properties
+  }
+}
---
+++ test/gov/ca/cwds/jenkins/semver/ManifestUpdaterSpecification.groovy
@@ -0,0 +1,64 @@
+package gov.ca.cwds.jenkins.semver
+
+import spock.lang.Specification
+
+class ManifestUpdaterSpecification extends Specification {
+  class PipeLineScript {
+
+    def sshagent(hash, closure) { }
+
+    def sh(hash) { }
+
+    def git(hash) { }
+
+    def readYaml(hash) { }
+
+    def writeYaml(hash) { }
+
+    def PipeLineScript() { }
+  }
+
+  def "#update writes to the yaml file"() {
+    given: "a manifest updater"
+    def PipeLineScript pipeline = Mock(PipeLineScript)
+    def manifestUpdater = new ManifestUpdater(pipeline)
+
+    when: "updating dashboard in preint"
+    manifestUpdater.update("dashboard", "preint", "cr-01", "1.3.0")
+
+    then: "it updates the file and commits the change"
+    1 * pipeline.git([branch: "master", credentialsId: "cr-01", url: "git@github.com:ca-cwds/cws-cares.git"])
+    1 * pipeline.readYaml([file: "preint.yaml"]) >> [dashboard: "1.2.4", cans: "1.4.5"]
+    1 * pipeline.writeYaml([file: "preint.yaml", data: [dashboard: "1.3.0", cans: "1.4.5"]])
+    1 * pipeline.sshagent(credentials: "cr-01", _ as Closure)
+  }
+
+  def "#commitVersionInCares commits the update"() {
+    given: "a manifest updater"
+    def PipeLineScript pipeline = Mock(PipeLineScript)
+    def manifestUpdater = new ManifestUpdater(pipeline)
+
+    when: "commiting the change in CARES"
+    manifestUpdater.commitVersionInCares()
+
+    then: "it commits as the Jenkins user"
+    1 * pipeline.sh([script: "git status --porcelain", returnStdout: true]) >> "?? Readme.md"
+    1 * pipeline.sh([script: "git config --global user.email cwdsdoeteam@osi.ca.gov"])
+    1 * pipeline.sh([script: "git config --global user.name 'Jenkins'"])
+    1 * pipeline.sh([script: "git commit -am \"Update component version from script\""])
+    1 * pipeline.sh([script: "git push origin master"])
+  }
+
+  def "#commitVersionInCares does nothing if the version hasn't changed"() {
+    given: "a manifest updater"
+    def PipeLineScript pipeline = Mock(PipeLineScript)
+    def manifestUpdater = new ManifestUpdater(pipeline)
+
+    when: "commiting the change in CARES"
+    manifestUpdater.commitVersionInCares()
+
+    then: "it noops"
+    1 * pipeline.sh([script: "git status --porcelain", returnStdout: true]) >> null
+    0 * pipeline.sh(_)
+  }
+}
