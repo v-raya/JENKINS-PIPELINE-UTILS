41422315296450b486574ddc39cc1dd37eaa3994
Ed Gibbs <edward_gibbs@yahoo.com>
2018-11-10 06:57:44 -0800

    Cleanup for some of the entrypoint scripts :unicorn:

--- vars/checkForLabel.groovy
+++ vars/checkForLabel.groovy
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env groovy
 
 import gov.ca.cwds.jenkins.semver.LabelChecker
 
--- vars/manifestUpdater.groovy
+++ vars/manifestUpdater.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
+import gov.ca.cwds.jenkins.ManifestUpdater
+
 def call(applicationName, manifestName, credentialsId, version) {
   manifestUpdater = new ManifestUpdater(this)
   manifestUpdater.update(applicationName, manifestName, credentialsId, version)
