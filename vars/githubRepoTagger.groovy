c78ccf2e2e6ec5ff6dc3e4b121cb4339f6a268e5
Ed Gibbs <edward_gibbs@yahoo.com>
2018-11-10 06:49:48 -0800

    Imports GithubRepoTagger so it can work :unicorn:

--- vars/githubRepoTagger.groovy
+++ vars/githubRepoTagger.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
+import gov.ca.cwds.jenkins.GithubRepoTagger
+
 def call(String tag, String credentials) {
   githubRepoTagger = new GithubRepoTagger(this)
   githubRepoTagger.tagWith(tag, credentials)
