265e04105c4d41a9c69f7ab68c2f501fa9768e6b
Ed Gibbs <edward_gibbs@yahoo.com>
2018-11-11 05:42:08 -0800

    Swapping out for another algorithim :unicorn:

--- src/gov/ca/cwds/jenkins/semver/NewTagGenerator.groovy
+++ src/gov/ca/cwds/jenkins/semver/NewTagGenerator.groovy
@@ -10,7 +10,6 @@ class NewTagGenerator {
   def newTag(tags, increment) {
     def latestTag = mostRecentVersion(tags)
     script.echo "Latest Tag: ${latestTag}"
-    println "latestTag: ${latestTag}"
     def (major, minor, patch) = latestTag.tokenize(".").collect { it as Integer }
     switch (increment) {
       case IncrementTypes.MAJOR:
@@ -29,19 +28,45 @@ class NewTagGenerator {
     "$major.$minor.$patch"
   }
 
-  private String mostRecentVersion(tags) {
-    tags.max { a, b ->
-      def versionA = a.tokenize('.')
-      def versionB = b.tokenize('.')
-      def commonIndices = Math.min(versionA.size(), versionB.size())
-      for (int index = 0; index < commonIndices; ++index) {
-        def numberA = versionA[index].toInteger()
-        def numberB = versionB[index].toInteger()
-        if (numberA != numberB) {
-          return numberA <=> numberB
+  // private String mostRecentVersion(tags) {
+  //   tags.max { a, b ->
+  //     def versionA = a.tokenize('.')
+  //     def versionB = b.tokenize('.')
+  //     def commonIndices = Math.min(versionA.size(), versionB.size())
+  //     for (int index = 0; index < commonIndices; ++index) {
+  //       def numberA = versionA[index].toInteger()
+  //       def numberB = versionB[index].toInteger()
+  //       if (numberA != numberB) {
+  //         return numberA <=> numberB
+  //       }
+  //     }
+  //     versionA.size() <=> versionB.size()
+  //   }
+  // }
+
+  private String mostRecentVersion(List versions) {
+    def sorted = versions.sort(false) { a, b ->
+      List verA = a.tokenize('.')
+      List verB = b.tokenize('.')
+
+      def commonIndices = Math.min(verA.size(), verB.size())
+
+      for (int i = 0; i < commonIndices; ++i) {
+        def numA = verA[i].toInteger()
+        def numB = verB[i].toInteger()
+        println "comparing $numA and $numB"
+
+        if (numA != numB) {
+          return numA <=> numB
         }
       }
-      versionA.size() <=> versionB.size()
+
+      // If we got this far then all the common indices are identical, so whichever version is longer must be more recent
+      verA.size() <=> verB.size()
     }
+
+    println "sorted versions: $sorted"
+    sorted.last()
   }
+
 }
