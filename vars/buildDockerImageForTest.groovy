0d62b0a53aa28cd2c2c9180eba515007dd4040d5
osi-richbach <rich.bach@osi.ca.gov>
2018-11-13 13:49:46 -0800

    removed uneeded/unused param to call

--- vars/buildDockerImageForTest.groovy
+++ vars/buildDockerImageForTest.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
 import gov.ca.cwds.jenkins.docker.Docker
 
-def call(String pathToDockerFile) { 
+def call() {
+  def pathToDockerFile './docker/test/Dockerfile'
   echo 'building image for test'
   docker = new Docker(this, docker)
   docker.createTestingImage(pathToDockerFile)
