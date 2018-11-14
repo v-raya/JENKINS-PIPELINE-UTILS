089be06f2559847190d4f77464408a8abfb1660c
Mariam Ghori <mariam.ghori@osi.ca.gov>
2018-11-14 11:28:47 -0800

    FIT-237 Add Hooks for Smoke Tests in Jenkins File for Dashboard Project

--- src/gov/ca/cwds/jenkins/SmokeTester.groovy
+++ src/gov/ca/cwds/jenkins/SmokeTester.groovy
@@ -8,13 +8,17 @@ class SmokeTester {
   }
   
   def runSmokeTest(path) {
-    def test = path.execute().text
-    if (test.contains("smoketest failed")) {
-        script.error ("'${test}'")
-        return "smoke test failed"
+    
+    def url = script.SMOKE_TEST_URL ?: "test"
+    def cmd = [path, url]
+    def test = cmd.execute().text
+    if (test.contains("smoketest passed")) {
+        script.echo "smoke test passed"
+        return "smoke test passed"
     }
-    script.echo "smoke test passed"
-    return "smoke test passed"
+    script.error ("'${test}'")
+    script.echo "smoke test failed"
+    return "smoke test failed"
   }
   
 }
