ffac62dcfafaf5512aaaeb7fd18bd3b2a7465cbb
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-14 12:45:43 -0800

    FIT-229: Implement Shared Library for Automated Licensing Phase Checks

--- examples/licensing/Jenkinsfile22.groovy
+++ examples/licensing/Jenkinsfile22.groovy
@@ -1,7 +1,7 @@
 def BRANCH = 'master'
 def SSH_CRED_ID = '1db97a1a-6604-4d90-9790-a0fd931af6f4'
 
-@Library('jenkins_pipeline_utils@master') _
+@Library('jenkins-pipeline-utils@master') _
 
 def sshAgent = new gov.ca.cwds.jenkins.SshAgent(this, SSH_CRED_ID)
 def licensingSupport = new gov.ca.cwds.jenkins.licensing.LicensingSupport(this, BRANCH, sshAgent)
