3a1250baaa02356825c1e72fed8584ef3493925f
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-14 18:16:23 -0800

    FIT-229: Implement Shared Library for Automated Licensing Phase Checks - added more automated tests

--- test/gov/ca/cwds/jenkins/LicensingSupportSpecification.groovy
+++ test/gov/ca/cwds/jenkins/LicensingSupportSpecification.groovy
@@ -9,20 +9,14 @@ import static gov.ca.cwds.jenkins.licensing.LicensingSupportUtils.MSG_NO_LICENSI
 class LicensingSupportSpecification extends Specification {
 
     class PipeLineScript {
-        def behaviour = [
-                sh : [:] // map of script -> result values where script is a parameter of the pipeline.sh() method
-        ]
-        def actualValues = [
-            lastShScriptCalled : null,
-            echoedMessages : [],
-            usedCredentialsId : null
-        ]
+        def spec
 
-        PipeLineScript() {
+        PipeLineScript(spec) {
+            this.spec = spec
         }
 
         def sh(String script) {
-            actualValues.lastShScriptCalled = script
+            spec.actualValues.calledShScripts.add(script)
             return behaviour && behaviour.sh ? behaviour.sh[script] : 1
         }
 
@@ -31,7 +25,7 @@ class LicensingSupportSpecification extends Specification {
         }
 
         def echo(String msg) {
-            actualValues.echoedMessages.add(msg)
+            spec.actualValues.echoedMessages.add(msg)
         }
 
         def readFile(Map params) {
@@ -41,32 +35,75 @@ class LicensingSupportSpecification extends Specification {
         }
 
         def sshagent(Map params, closure) {
-            actualValues.usedCredentialsId = params.credentials[0]
+            spec.actualValues.usedCredentialsId = params.credentials[0]
             closure()
-        }
-
-        // assertion methods
-
-        def isLastShScriptCalled(String expectedScript) {
-            expectedScript == actualValues.lastShScriptCalled
-        }
-
-        def isMessageEchoed(String expectedMessage) {
-            actualValues.echoedMessages.contains(expectedMessage)
-        }
-
-        def isCredentialsIdUsed(String expectedCredentialsId) {
-            expectedCredentialsId == actualValues.usedCredentialsId
         }
     }
 
     class GradleRuntime {
-        def Map lastRunParameters
+        def spec
+
+        def GradleRuntime(spec) {
+            this.spec = spec
+        }
 
         def run(Map parameters) {
-            lastRunParameters = parameters
+            spec.actualValues.lastGradleRuntimeParameters = parameters
         }
     }
+
+    def behaviour = [
+            sh: [:] // map of script -> result values where script is a parameter of the pipeline.sh() method
+    ]
+
+    def actualValues = [
+            calledShScripts            : [] as Set,
+            echoedMessages             : [] as Set,
+            usedCredentialsId          : null,
+            lastGradleRuntimeParameters: null
+    ]
+
+    static def getSshGitCommand(String gitCommand) {
+        'GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" ' + gitCommand
+    }
+
+    final static def SSH_GIT_CONFIG_USER = getSshGitCommand('git config --global user.name Jenkins')
+    final static def SSH_GIT_CONFIG_EMAIL = getSshGitCommand('git config --global user.email cwdsdoeteam@osi.ca.gov')
+    final static def SSH_GIT_ADD_LEGAL = getSshGitCommand('git add legal')
+    final static def SSH_GIT_COMMIT = getSshGitCommand('git commit -m "updated license info"')
+    final static def SSH_GIT_PUSH = getSshGitCommand('git push --set-upstream origin master')
+
+    def setUpGitSshCommands() {
+        behaviour.sh[SSH_GIT_CONFIG_USER] = 0
+        behaviour.sh[SSH_GIT_CONFIG_EMAIL] = 0
+        behaviour.sh[SSH_GIT_ADD_LEGAL] = 0
+        behaviour.sh[SSH_GIT_COMMIT] = 0
+        behaviour.sh[SSH_GIT_PUSH] = 0
+    }
+
+    // assertion methods
+
+    def isLastShScriptCalled(String expectedShScript) {
+        expectedShScript == actualValues.calledShScripts.last()
+    }
+
+    def areShScriptsCalled(Set expectedShScripts) {
+        expectedShScripts == actualValues.calledShScripts
+    }
+
+    def isMessageEchoed(String expectedMessage) {
+        actualValues.echoedMessages.contains(expectedMessage)
+    }
+
+    def isCredentialsIdUsed(String expectedCredentialsId) {
+        expectedCredentialsId == actualValues.usedCredentialsId
+    }
+
+    def areLastGradleRuntimeParameters(expectedParameters) {
+        expectedParameters == actualValues.lastGradleRuntimeParameters
+    }
+
+    // test methods
 
     def "When can't detect LicensingSupportType then generateLicenseReport throws Exception"() {
         given:
@@ -101,17 +138,16 @@ class LicensingSupportSpecification extends Specification {
     def "When can't execute ssh git command then pushLicenseReport throws Exception"() {
         given:
         final def gitCommand = 'git config --global user.name Jenkins'
-        final def sshGitCommand = "GIT_SSH_COMMAND=\"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\" ${gitCommand}"
-        def pipeline = new PipeLineScript()
-        pipeline.behaviour = [
-                sh : [
-                        'grep -c "com.github.hierynomus.license" build.gradle' : 0,
-                        'test -e build.gradle' : 0,
-                        'test -e package.json' : 1,
-                        'grep -c "license_finder" package.json' : 1,
-                        "${sshGitCommand}" : 1
+        behaviour = [
+                sh: [
+                        'test -e build.gradle'                                : 0,
+                        'grep -c "com.github.hierynomus.license" build.gradle': 0,
+                        'test -e package.json'                                : 1,
+                        'grep -c "license_finder" package.json'               : 1,
+                        "${SSH_GIT_CONFIG_USER}"                              : 1
                 ]
         ]
+        def pipeline = new PipeLineScript(this)
         def sshAgent = new SshAgent(pipeline, 'credentials-id')
         def licensingSupport = new LicensingSupport(pipeline, 'master', sshAgent)
 
@@ -119,92 +155,133 @@ class LicensingSupportSpecification extends Specification {
         licensingSupport.pushLicenseReport()
 
         then:
-        pipeline.isCredentialsIdUsed('credentials-id')
-        pipeline.isLastShScriptCalled(sshGitCommand)
+        isCredentialsIdUsed('credentials-id')
+        isLastShScriptCalled(SSH_GIT_CONFIG_USER)
         def exception = thrown(Exception)
         exception.message == "ssh command '${gitCommand}' failed"
     }
 
     def "When build is not from the master branch then skip license report generation"() {
         given:
-        def pipeline = new PipeLineScript()
+        def pipeline = new PipeLineScript(this)
         def licensingSupport = new LicensingSupport(pipeline, 'myTempBranch', null)
 
         when:
         licensingSupport.generateAndPushLicenseReport()
 
         then:
-        pipeline.isMessageEchoed('Not working with the master branch. Skipping License Generation for the other branch.')
-        pipeline.isMessageEchoed('Not working with the master branch. Skipping Push License Report for the other branch.')
+        isMessageEchoed('Not working with the master branch. Skipping License Generation for the other branch.')
+        isMessageEchoed('Not working with the master branch. Skipping Push License Report for the other branch.')
     }
-
 
     def "When it is the back-end project with gradle and it uses hierynomus license then gradlew is called to generate license report"() {
         given:
-        def pipeline = new PipeLineScript()
-        pipeline.behaviour = [
-                sh : [
-                        'grep -c "com.github.hierynomus.license" build.gradle' : 0,
-                        'test -e build.gradle' : 0,
-                        'test -e package.json' : 1,
-                        'grep -c "license_finder" package.json' : 1
+        behaviour = [
+                sh: [
+                        'test -e build.gradle'                                : 0,
+                        'grep -c "com.github.hierynomus.license" build.gradle': 0,
+                        'test -e package.json'                                : 1,
+                        'grep -c "license_finder" package.json'               : 1
                 ]
         ]
-        def licensingSupport = new LicensingSupport(pipeline, 'master', null)
+        setUpGitSshCommands()
+        def pipeline = new PipeLineScript(this)
+        def sshAgent = new SshAgent(pipeline, 'credentials-id')
+        def licensingSupport = new LicensingSupport(pipeline, 'master', sshAgent)
 
         when:
-        licensingSupport.generateLicenseReport()
+        licensingSupport.generateAndPushLicenseReport()
 
         then:
-        pipeline.isLastShScriptCalled('./gradlew deleteLicenses downloadLicenses copyLicenses')
-        pipeline.isMessageEchoed('Detected Licensing Support Type: Gradle Hierynomus License Plugin')
-        pipeline.isMessageEchoed('Generating License Information')
+        isCredentialsIdUsed('credentials-id')
+        def expectedShScriptsCalled = [
+                'test -e build.gradle',
+                'grep -c "com.github.hierynomus.license" build.gradle',
+                './gradlew deleteLicenses downloadLicenses copyLicenses',
+                SSH_GIT_CONFIG_USER,
+                SSH_GIT_CONFIG_EMAIL,
+                SSH_GIT_ADD_LEGAL,
+                SSH_GIT_COMMIT,
+                SSH_GIT_PUSH
+        ] as Set
+        areShScriptsCalled(expectedShScriptsCalled)
+        isMessageEchoed('Detected Licensing Support Type: Gradle Hierynomus License Plugin')
+        isMessageEchoed('Generating License Information')
+        isMessageEchoed('Updating License Information')
     }
 
     def "When a GradleRuntime is provided and it is the back-end project with gradle and it uses hierynomus license then the GradleRuntime is called to generate license report"() {
         given:
-        def pipeline = new PipeLineScript()
-        pipeline.behaviour = [
-                sh : [
-                        'grep -c "com.github.hierynomus.license" build.gradle' : 0,
-                        'test -e build.gradle' : 0,
-                        'test -e package.json' : 1,
-                        'grep -c "license_finder" package.json' : 1
+        behaviour = [
+                sh: [
+                        'test -e build.gradle'                                : 0,
+                        'grep -c "com.github.hierynomus.license" build.gradle': 0,
+                        'test -e package.json'                                : 1,
+                        'grep -c "license_finder" package.json'               : 1
                 ]
         ]
-        def licensingSupport = new LicensingSupport(pipeline, 'master', null)
-        def gradleRuntime = new GradleRuntime()
+        setUpGitSshCommands()
+        def pipeline = new PipeLineScript(this)
+        def sshAgent = new SshAgent(pipeline, 'credentials-id')
+        def licensingSupport = new LicensingSupport(pipeline, 'master', sshAgent)
+        def gradleRuntime = new GradleRuntime(this)
         licensingSupport.gradleRuntime = gradleRuntime
 
         when:
-        licensingSupport.generateLicenseReport()
+        licensingSupport.generateAndPushLicenseReport()
 
         then:
-        pipeline.isLastShScriptCalled('grep -c "com.github.hierynomus.license" build.gradle')
-        [buildFile: 'build.gradle', tasks: 'deleteLicenses downloadLicenses copyLicenses'] == gradleRuntime.lastRunParameters
-        pipeline.isMessageEchoed('Detected Licensing Support Type: Gradle Hierynomus License Plugin')
-        pipeline.isMessageEchoed('Generating License Information')
+        isCredentialsIdUsed('credentials-id')
+        def expectedShScriptsCalled = [
+                'test -e build.gradle',
+                'grep -c "com.github.hierynomus.license" build.gradle',
+                SSH_GIT_CONFIG_USER,
+                SSH_GIT_CONFIG_EMAIL,
+                SSH_GIT_ADD_LEGAL,
+                SSH_GIT_COMMIT,
+                SSH_GIT_PUSH
+        ] as Set
+        areShScriptsCalled(expectedShScriptsCalled)
+        areLastGradleRuntimeParameters([buildFile: 'build.gradle', tasks: 'deleteLicenses downloadLicenses copyLicenses'])
+        isMessageEchoed('Detected Licensing Support Type: Gradle Hierynomus License Plugin')
+        isMessageEchoed('Generating License Information')
+        isMessageEchoed('Updating License Information')
     }
 
     def "When it is the front-end project with package.json and it uses license finder then yarn is called to generate license report"() {
         given:
-        def pipeline = new PipeLineScript()
-        pipeline.behaviour = [
-                sh : [
-                        'grep -c "com.github.hierynomus.license" build.gradle' : 1,
-                        'test -e build.gradle' : 1,
-                        'test -e package.json' : 0,
-                        'grep -c "license_finder" package.json' : 0
+        behaviour = [
+                sh: [
+                        'test -e build.gradle'                                : 1,
+                        'grep -c "com.github.hierynomus.license" build.gradle': 1,
+                        'test -e package.json'                                : 0,
+                        'grep -c "license_finder" package.json'               : 0
                 ]
         ]
-        def licensingSupport = new LicensingSupport(pipeline, 'master', null)
+        setUpGitSshCommands()
+        def pipeline = new PipeLineScript(this)
+        def sshAgent = new SshAgent(pipeline, 'credentials-id')
+        def licensingSupport = new LicensingSupport(pipeline, 'master', sshAgent)
 
         when:
-        licensingSupport.generateLicenseReport()
+        licensingSupport.generateAndPushLicenseReport()
 
         then:
-        pipeline.isLastShScriptCalled('yarn licenses-report')
-        pipeline.isMessageEchoed('Detected Licensing Support Type: Ruby License Finder Plugin')
-        pipeline.isMessageEchoed('Generating License Information')
+        isCredentialsIdUsed('credentials-id')
+        def expectedShScriptsCalled = [
+                'test -e build.gradle',
+                'test -e package.json',
+                'grep -c "license_finder" package.json',
+                'yarn licenses-report',
+                SSH_GIT_CONFIG_USER,
+                SSH_GIT_CONFIG_EMAIL,
+                SSH_GIT_ADD_LEGAL,
+                SSH_GIT_COMMIT,
+                SSH_GIT_PUSH
+        ] as Set
+        areShScriptsCalled(expectedShScriptsCalled)
+        isMessageEchoed('Detected Licensing Support Type: Ruby License Finder Plugin')
+        isMessageEchoed('Generating License Information')
+        isMessageEchoed('Updating License Information')
     }
 }
