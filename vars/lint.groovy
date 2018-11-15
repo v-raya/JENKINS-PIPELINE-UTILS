2618c8dcd110cce4f7a44c5a1e2139cd83f3073a
balutbomber <balutbomber@gmail.com>
2018-11-15 14:14:44 -0800

    passing rtGradle down from jenkinsfile

--- vars/lint.groovy
+++ vars/lint.groovy
@@ -4,10 +4,9 @@ import gov.ca.cwds.jenkins.docker.Docker
 import gov.ca.cwds.jenkins.common.BuildMetadata
 
 
-def call() {  
+def call(rtGradle = null) {  
     BuildMetadata buildMetadata = new BuildMetadata(this, this.env.JOB_NAME, this.env.BUILD_ID, this.env.WORKSPACE)
     def docker = new Docker(this)
-    def rtGradle = Artifactory.newGradleBuild()
     staticAnalyzer = new StaticAnalyzer(docker, rtGradle, this)  
     staticAnalyzer.lint(buildMetadata);
 }
