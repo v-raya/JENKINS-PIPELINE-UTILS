10e3711b7795956733056eb3ae9487266a15e57a
Mariam Ghori <mariam.ghori@osi.ca.gov>
2018-11-14 19:05:00 -0800

    FIT-237 Add Hooks for Smoke Tests in Jenkins File for Dashboard Project

--- test/gov/ca/cwds/jenkins/SmokeTesterSpecification.groovy
+++ test/gov/ca/cwds/jenkins/SmokeTesterSpecification.groovy
@@ -5,27 +5,38 @@ import spock.lang.Specification
 class SmokeTesterSpecification extends Specification {
 
   class PipeLineScript {
-    def build(hash) {
-    }
 
-    def PipeLineScript() { }
+    def error(hash) {}
+
+    def PipeLineScript() {}
   }
 
-  def "#runSmokeTest"() {
+  def "#runSmokeTestPasses"() {
 
     given:
     def pipeline = Mock(PipeLineScript)
     def passed = './test/resources/smoketest/passed.sh'
-    def failed = './test/resources/smoketest/failed.sh'
     def smokeTester = new SmokeTester(pipeline)
 
     when:
     def smokePassed = smokeTester.runSmokeTest(passed, "test")
-    def smokeFailed = smokeTester.runSmokeTest(failed, "test")
 
     then:
     smokePassed == "smoke test passed"
-    smokeFailed == null
-
   }
+
+  def "#runSmokeTestFails"() {
+
+    given:
+    def pipeline = Mock(PipeLineScript)
+    def failed = './test/resources/smoketest/failed.sh'
+    def smokeTester = new SmokeTester(pipeline)
+
+    when:
+    smokeTester.runSmokeTest(failed, "test")
+
+    then:
+    1 * pipeline.error(_)
+  }
+
 }
