1b2d3c53527609dbdd8728915f006f87de4eabd0
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-14 17:04:37 -0800

    FIT-229: Implement Shared Library for Automated Licensing Phase Checks - added more automated tests

--- test/gov/ca/cwds/jenkins/LicensingSupportSpecification.groovy
+++ test/gov/ca/cwds/jenkins/LicensingSupportSpecification.groovy
@@ -12,15 +12,17 @@ class LicensingSupportSpecification extends Specification {
         def behaviour = [
                 sh : [:] // map of script -> result values where script is a parameter of the pipeline.sh() method
         ]
-        def lastShScriptCalled = null
-
-        def echoedMessages = []
+        def actualValues = [
+            lastShScriptCalled : null,
+            echoedMessages : [],
+            usedCredentialsId : null
+        ]
 
         PipeLineScript() {
         }
 
         def sh(String script) {
-            lastShScriptCalled = script
+            actualValues.lastShScriptCalled = script
             return behaviour && behaviour.sh ? behaviour.sh[script] : 1
         }
 
@@ -29,7 +31,7 @@ class LicensingSupportSpecification extends Specification {
         }
 
         def echo(String msg) {
-            this.echoedMessages.add(msg)
+            actualValues.echoedMessages.add(msg)
         }
 
         def readFile(Map params) {
@@ -38,12 +40,23 @@ class LicensingSupportSpecification extends Specification {
         def writeFile(Map params) {
         }
 
-        def isLastShScriptCalled(String script) {
-            script == lastShScriptCalled
+        def sshagent(Map params, closure) {
+            actualValues.usedCredentialsId = params.credentials[0]
+            closure()
         }
 
-        def isMessageEchoed(String msg) {
-            echoedMessages.contains(msg)
+        // assertion methods
+
+        def isLastShScriptCalled(String expectedScript) {
+            expectedScript == actualValues.lastShScriptCalled
+        }
+
+        def isMessageEchoed(String expectedMessage) {
+            actualValues.echoedMessages.contains(expectedMessage)
+        }
+
+        def isCredentialsIdUsed(String expectedCredentialsId) {
+            expectedCredentialsId == actualValues.usedCredentialsId
         }
     }
 
@@ -55,7 +68,7 @@ class LicensingSupportSpecification extends Specification {
         }
     }
 
-    def "When can't detect LicensingSupportType then generateLicenseReport throw Exception"() {
+    def "When can't detect LicensingSupportType then generateLicenseReport throws Exception"() {
         given:
         def pipeline = Mock(PipeLineScript)
         def licensingSupport = new LicensingSupport(pipeline, 'master', null)
@@ -70,7 +83,7 @@ class LicensingSupportSpecification extends Specification {
         exception.message == MSG_NO_LICENSING_SUPPORT
     }
 
-    def "When can't detect LicensingSupportType then pushLicenseReport throw Exception"() {
+    def "When can't detect LicensingSupportType then pushLicenseReport throws Exception"() {
         given:
         def pipeline = Mock(PipeLineScript)
         def licensingSupport = new LicensingSupport(pipeline, 'master', null)
@@ -83,6 +96,33 @@ class LicensingSupportSpecification extends Specification {
         then:
         def exception = thrown(Exception)
         exception.message == MSG_NO_LICENSING_SUPPORT
+    }
+
+    def "When can't execute ssh git command then pushLicenseReport throws Exception"() {
+        given:
+        final def gitCommand = 'git config --global user.name Jenkins'
+        final def sshGitCommand = "GIT_SSH_COMMAND=\"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\" ${gitCommand}"
+        def pipeline = new PipeLineScript()
+        pipeline.behaviour = [
+                sh : [
+                        'grep -c "com.github.hierynomus.license" build.gradle' : 0,
+                        'test -e build.gradle' : 0,
+                        'test -e package.json' : 1,
+                        'grep -c "license_finder" package.json' : 1,
+                        "${sshGitCommand}" : 1
+                ]
+        ]
+        def sshAgent = new SshAgent(pipeline, 'credentials-id')
+        def licensingSupport = new LicensingSupport(pipeline, 'master', sshAgent)
+
+        when:
+        licensingSupport.pushLicenseReport()
+
+        then:
+        pipeline.isCredentialsIdUsed('credentials-id')
+        pipeline.isLastShScriptCalled(sshGitCommand)
+        def exception = thrown(Exception)
+        exception.message == "ssh command '${gitCommand}' failed"
     }
 
     def "When build is not from the master branch then skip license report generation"() {
