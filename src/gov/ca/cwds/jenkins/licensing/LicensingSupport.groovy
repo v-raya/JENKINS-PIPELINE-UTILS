08ff922a8192bf618ac54b94c6067036b6f98999
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-15 16:05:12 -0800

    FIT-229: Implement Shared Library for Automated Licensing Phase Checks - dropped redundant this. references

--- src/gov/ca/cwds/jenkins/licensing/LicensingSupport.groovy
+++ src/gov/ca/cwds/jenkins/licensing/LicensingSupport.groovy
@@ -19,31 +19,31 @@ class LicensingSupport implements Serializable {
   }
 
   def initLicensingSupportType() {
-    if (null == this.licensingSupportType) {
-      this.licensingSupportType = LicensingSupportUtils.getLicensingSupportType(pipeline)
-      this.pipeline.echo("Detected Licensing Support Type: ${this.licensingSupportType.title}")
+    if (null == licensingSupportType) {
+      licensingSupportType = LicensingSupportUtils.getLicensingSupportType(pipeline)
+      pipeline.echo("Detected Licensing Support Type: ${licensingSupportType.title}")
     }
-    if (LicensingSupportType.NONE == this.licensingSupportType) {
+    if (LicensingSupportType.NONE == licensingSupportType) {
       throw new Exception(MSG_NO_LICENSING_SUPPORT)
     }
   }
 
   def generateLicenseReport() {
-    if ('master' == this.branchName) {
+    if ('master' == branchName) {
       initLicensingSupportType()
       pipeline.echo 'Generating License Information'
-      switch (this.licensingSupportType) {
+      switch (licensingSupportType) {
         case LicensingSupportType.GRADLE_HIERYNOMUS_LICENSE:
-          LicensingSupportUtils.addLicensingGradleTasks(this.pipeline)
-          if (null == this.gradleRuntime) {
-            this.pipeline.sh './gradlew deleteLicenses downloadLicenses copyLicenses'
+          LicensingSupportUtils.addLicensingGradleTasks(pipeline)
+          if (null == gradleRuntime) {
+            pipeline.sh './gradlew deleteLicenses downloadLicenses copyLicenses'
           } else {
-            this.gradleRuntime.run buildFile: 'build.gradle',
+            gradleRuntime.run buildFile: 'build.gradle',
               tasks: 'deleteLicenses downloadLicenses copyLicenses'
           }
           break
         case LicensingSupportType.RUBY_LICENSE_FINDER:
-          this.pipeline.sh 'yarn licenses-report'
+          pipeline.sh 'yarn licenses-report'
           break
       }
     } else {
@@ -52,17 +52,17 @@ class LicensingSupport implements Serializable {
   }
 
   def pushLicenseReport() {
-    if ('master' == this.branchName) {
+    if ('master' == branchName) {
       initLicensingSupportType()
       pipeline.echo 'Updating License Information'
-      switch (this.licensingSupportType) {
+      switch (licensingSupportType) {
         case LicensingSupportType.GRADLE_HIERYNOMUS_LICENSE:
         case LicensingSupportType.RUBY_LICENSE_FINDER:
-          this.sshAgent.run("git config --global user.name ${GIT_USER}", true)
-          this.sshAgent.run("git config --global user.email ${GIT_EMAIL}", true)
-          this.sshAgent.run("git add ${LICENSE_FOLDER}")
-          this.sshAgent.run('git commit -m "updated license info"')
-          this.sshAgent.run('git push --set-upstream origin master', true)
+          sshAgent.run("git config --global user.name ${GIT_USER}", true)
+          sshAgent.run("git config --global user.email ${GIT_EMAIL}", true)
+          sshAgent.run("git add ${LICENSE_FOLDER}")
+          sshAgent.run('git commit -m "updated license info"')
+          sshAgent.run('git push --set-upstream origin master', true)
           break
       }
     } else {
