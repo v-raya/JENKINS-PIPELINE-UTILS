7b0dbae6e71d162b7770593c501ab005574cc3ff
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-16 18:05:42 -0800

    (FIT-229) - refactor

--- src/gov/ca/cwds/jenkins/licensing/LicensingSupport.groovy
+++ src/gov/ca/cwds/jenkins/licensing/LicensingSupport.groovy
@@ -1,12 +1,6 @@
 package gov.ca.cwds.jenkins.licensing
 
-import static gov.ca.cwds.jenkins.licensing.LicensingSupportUtils.LICENSE_FOLDER
-import static gov.ca.cwds.jenkins.licensing.LicensingSupportUtils.MSG_NO_LICENSING_SUPPORT
-
 class LicensingSupport {
-  def GIT_USER = 'Jenkins'
-  def GIT_EMAIL = 'cwdsdoeteam@osi.ca.gov'
-
   def pipeline
   def licensingSupportType = null
 
@@ -25,46 +19,42 @@ class LicensingSupport {
   }
 
   private def initLicensingSupportType() {
-    if (null == licensingSupportType) {
-      licensingSupportType = LicensingSupportUtils.getLicensingSupportType(pipeline)
-      pipeline.echo("Detected Licensing Support Type: ${licensingSupportType.title}")
-    }
+    licensingSupportType = new LicensingSupportTypeDeterminer(pipeline).determineLicensingSupportType()
+    pipeline.echo("Detected Licensing Support Type: ${licensingSupportType.title}")
     if (LicensingSupportType.NONE == licensingSupportType) {
-      throw new Exception(MSG_NO_LICENSING_SUPPORT)
+      throw new Exception(LicensingSupportConstants.MSG_NO_LICENSING_SUPPORT)
     }
   }
 
   private def generateLicenseReport(gradleRuntime = null) {
     pipeline.echo 'Generating License Information'
-    switch (licensingSupportType) {
-      case LicensingSupportType.GRADLE_HIERYNOMUS_LICENSE:
-        LicensingSupportUtils.addLicensingGradleTasks(pipeline)
-        if (null == gradleRuntime) {
-          pipeline.sh './gradlew deleteLicenses downloadLicenses copyLicenses'
-        } else {
-          gradleRuntime.run buildFile: 'build.gradle',
-            tasks: 'deleteLicenses downloadLicenses copyLicenses'
-        }
-        break
-      case LicensingSupportType.RUBY_LICENSE_FINDER:
-        pipeline.sh 'yarn licenses-report'
-        break
+    if (licensingSupportType == LicensingSupportType.GRADLE_HIERYNOMUS_LICENSE) {
+      addLicensingGradleTasks(pipeline)
+      if (null == gradleRuntime) {
+        pipeline.sh './gradlew deleteLicenses downloadLicenses copyLicenses'
+      } else {
+        gradleRuntime.run buildFile: 'build.gradle',
+          tasks: 'deleteLicenses downloadLicenses copyLicenses'
+      }
+    } else if (licensingSupportType == LicensingSupportType.RUBY_LICENSE_FINDER) {
+      pipeline.sh 'yarn licenses-report'
     }
+  }
+
+  private def addLicensingGradleTasks(pipeline) {
+    def source = pipeline.readFile file: 'build.gradle'
+    source += LicensingSupportConstants.ADDITIONAL_LICENSING_GRADLE_TASKS
+    pipeline.writeFile file: 'build.gradle', text: "$source"
   }
 
   private def pushLicenseReport(sshCredentialsId) {
     pipeline.echo 'Updating License Information'
-    switch (licensingSupportType) {
-      case LicensingSupportType.GRADLE_HIERYNOMUS_LICENSE:
-      case LicensingSupportType.RUBY_LICENSE_FINDER:
-        pipeline.sshagent(credentials: [sshCredentialsId]) {
-          runSshCommand("git config --global user.name ${GIT_USER}", true)
-          runSshCommand("git config --global user.email ${GIT_EMAIL}", true)
-          runSshCommand("git add ${LICENSE_FOLDER}")
-          runSshCommand('git commit -m "updated license info"')
-          runSshCommand('git push --set-upstream origin master', true)
-        }
-        break
+    pipeline.sshagent(credentials: [sshCredentialsId]) {
+      runSshCommand("git config --global user.name ${LicensingSupportConstants.GIT_USER}", true)
+      runSshCommand("git config --global user.email ${LicensingSupportConstants.GIT_EMAIL}", true)
+      runSshCommand("git add ${LicensingSupportConstants.LICENSE_FOLDER}")
+      runSshCommand('git commit -m "updated license info"')
+      runSshCommand('git push --set-upstream origin master', true)
     }
   }
 
