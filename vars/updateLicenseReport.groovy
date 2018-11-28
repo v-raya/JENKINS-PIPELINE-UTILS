71982cd5d0f1dea8ec65ea76b11362237584f81e
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-27 17:18:04 -0800

    (FIT-229) - updated documentation

--- vars/updateLicenseReport.groovy
+++ vars/updateLicenseReport.groovy
@@ -2,15 +2,6 @@ package gov.ca.cwds.jenkins
 
 import gov.ca.cwds.jenkins.licensing.LicensingSupport
 
-/*
-Usage in Jenkins pipeline:
-
-stage('Update License Report') {
-  updateLicenseReport(branch, sshCredentialsId, runtimeGradle)
-}
-
-gradleRuntime is optional
- */
 def call(branchName, sshCredentialsId, runtimeGradle = null, dockerImage = null) {
   def licensingSupport = new LicensingSupport(this, runtimeGradle, dockerImage)
   licensingSupport.updateLicenseReport(branchName, sshCredentialsId)
