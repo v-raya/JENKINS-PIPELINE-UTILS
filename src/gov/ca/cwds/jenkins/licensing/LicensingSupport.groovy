ebbd0201e54424819f0b4e672b22c7531bd99c45
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-27 23:06:00 -0800

    (FIT-229) - fix for front end projects

--- src/gov/ca/cwds/jenkins/licensing/LicensingSupport.groovy
+++ src/gov/ca/cwds/jenkins/licensing/LicensingSupport.groovy
@@ -43,6 +43,7 @@ class LicensingSupport {
       def LICENSES_FILE = "${LICENSE_FOLDER}/licenses.csv"
       pipeline.sh script: "mkdir ${LICENSE_FOLDER}", returnStatus: true
       dockerImage.withRun("-e CI=true") { container ->
+        pipeline.echo("************** " + container.toString())
         pipeline.sh script: "docker exec -t ${container.id} mkdir ${LICENSE_FOLDER}", returnStatus: true
         //pipeline.sh "docker exec -t ${container.id} touch ${LICENSES_FILE}"
         pipeline.sh "docker exec -t ${container.id} yarn licenses-report"
