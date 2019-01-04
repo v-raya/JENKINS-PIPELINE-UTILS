b9a8fe5c56006c6ced50d670ccbf6c0bbe938505
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2019-01-03 17:20:28 -0800

    (FIT-354) - updated label checker, so it can check for tag prefix labels in a PR

--- src/gov/ca/cwds/jenkins/semver/LabelChecker.groovy
+++ src/gov/ca/cwds/jenkins/semver/LabelChecker.groovy
@@ -7,10 +7,13 @@ class LabelChecker {
       this.script = script
     }
 
-  def check(projectName) {
+  def check(projectName, List tagPrefixes = []) {
     def pullRequestUrl = "https://api.github.com/repos/ca-cwds/${projectName}/issues/${script.env.ghprbPullId}/labels"
     def response = pullRequestUrl.toURL().text
     def labels = script.readJSON(text: response).collect { it.name }
+    if (tagPrefixes) {
+      new TagPrefixFinder(tagPrefixes).find(labels)
+    }
     new VersionIncrement().increment(labels)
   }
 }
--- test/gov/ca/cwds/jenkins/semver/LabelCheckerSpecification.groovy
+++ test/gov/ca/cwds/jenkins/semver/LabelCheckerSpecification.groovy
@@ -21,7 +21,7 @@ class LabelCheckerSpecification extends Specification {
     }
   }
 
-  def "#check with proper labels"() {
+  def "#check with proper increment label"() {
     given: "a label checker"
     def environment = Mock(Environment)
     def pipeline = Mock(PipeLineScript)
@@ -30,7 +30,7 @@ class LabelCheckerSpecification extends Specification {
     def url = GroovyMock(URL)
     def responseText = '[{"name":"minor"}]'
 
-    when: "checking with a good label"
+    when: "checking with a good increment label"
     def increment = labelChecker.check("dashboard")
 
     then: "it does not throw and error and returns the increment"
@@ -42,7 +42,7 @@ class LabelCheckerSpecification extends Specification {
     increment == IncrementTypes.MINOR
   }
 
-  def "#check without proper labels"() {
+  def "#check without proper increment label"() {
     given: "a label checker"
     def environment = Mock(Environment)
     def pipeline = Mock(PipeLineScript)
@@ -51,7 +51,7 @@ class LabelCheckerSpecification extends Specification {
     def url = GroovyMock(URL)
     def responseText = '[]'
 
-    when: "checking with no label"
+    when: "checking with no increment label"
     labelChecker.check("dashboard")
 
     then: "it throws an exception"
@@ -62,5 +62,48 @@ class LabelCheckerSpecification extends Specification {
     1 * pipeline.readJSON([text: responseText]) >> []
     def error = thrown(Exception)
     error.message == "No labels found. Please label PR with 'major', 'minor', or 'patch'"
+  }
+
+  def "#check with a tag prefix label"() {
+    given: "a label checker"
+    def environment = Mock(Environment)
+    def pipeline = Mock(PipeLineScript)
+    def labelChecker = new LabelChecker(pipeline)
+    def org.codehaus.groovy.runtime.GStringImpl anyString = GroovyMock(global: true)
+    def url = GroovyMock(URL)
+    def responseText = '[{"name":"minor"},{"name":"lis"}]'
+
+    when: "checking with a tag prefix label"
+    def increment = labelChecker.check("dashboard", ['lis', 'cwscms', 'capu'])
+
+    then: "it does not throw and error and returns the increment"
+    1 * pipeline.env >> environment
+    1 * environment.ghprbPullId >> 122
+    1 * anyString.toURL() >> url
+    1 * url.text >> responseText
+    1 * pipeline.readJSON([text: responseText]) >> [[name: 'minor'], [name: 'lis']]
+    increment == IncrementTypes.MINOR
+  }
+
+  def "#check without a tag prefix label"() {
+    given: "a label checker"
+    def environment = Mock(Environment)
+    def pipeline = Mock(PipeLineScript)
+    def labelChecker = new LabelChecker(pipeline)
+    def org.codehaus.groovy.runtime.GStringImpl anyString = GroovyMock(global: true)
+    def url = GroovyMock(URL)
+    def responseText = '[{"name":"minor"}]'
+
+    when: "checking with no tag prefix label"
+    labelChecker.check("dashboard", ['lis', 'cwscms', 'capu'])
+
+    then: "it throws an exception"
+    1 * pipeline.env >> environment
+    1 * environment.ghprbPullId >> 122
+    1 * anyString.toURL() >> url
+    1 * url.text >> responseText
+    1 * pipeline.readJSON([text: responseText]) >> [[name: 'minor']]
+    def error = thrown(Exception)
+    error.message == "No labels with tag prefix found. Please label PR with one of [lis, cwscms, capu]"
   }
 }
